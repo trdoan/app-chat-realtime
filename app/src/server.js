5c027e1f2187fe49ae72f82133b10ba8cc88e884
trdoan <tranduongdoan0922@gmail.com>
2022-05-02 13:47:51 +0700

    backup code 02.05.2022

--- app/src/server.js
+++ app/src/server.js
@@ -6,6 +6,7 @@ const { Server } = require("socket.io");
 const { getListUsersByRoom, addUser, removeUser } = require("./model/user");
 const { rootRouter } = require("../../router/rootRouter");
 const cors = require("cors");
+
 const pathPublicDirectory = path.join(__dirname, "../public");
 const port = process.env.PORT || 5001;
 app.use(cors());
@@ -13,25 +14,45 @@ app.use(express.static(pathPublicDirectory));
 app.use(express.json());
 app.use("/", rootRouter);
 const httpServer = createServer(app);
+const { Sequelize } = require("sequelize");
+const sequelize = new Sequelize("SP9TV1670g", "SP9TV1670g", "bYYr6kVWnN", {
+  host: "remotemysql.com",
+  dialect: "mysql",
+});
 const io = new Server(httpServer, {
   cors: "*",
 });
-// co 1 ket noi moi
-io.on("connection", (socket) => {
+//
+io.on("connection", async (socket) => {
+  let time = new Date().toISOString().split("T")[0];
+
+  try {
+    // const [results, metadata] = await sequelize.query(
+    //   `INSERT INTO Rooms (name,hostId,createdAt,updatedAt) VALUES ("1",1,"${time}","${time}");`
+    // );
+    // console.log(results);
+  } catch (error) {
+    console.log(error);
+  }
+
+  console.log("a new socket connected" + socket.id);
   socket.emit("helloID", socket.id);
+  // có 1 phòng vừa được tạo trên server
   socket.on("new-room-created", (data) => {
+    // gửi tb tới client khác cập nhật FE
     io.emit("send-rooms-to-client");
   });
-  console.log(socket.id);
+
   let roomID;
+  // sự kiện user tham gia phòng
   socket.on("join-room", (data) => {
-    const { username, room, id } = data;
+    const { displayName, room, id } = data;
     roomID = room;
     // xu ly vao phong
     socket.join(room);
     const newUser = {
       id: socket.id,
-      username,
+      displayName,
       room,
     };
     console.log(newUser);
@@ -43,8 +64,8 @@ io.on("connection", (socket) => {
 
     // xin chao user moi khi vao room
     socket.emit("helloFirstTime", {
-      username: "Hệ thống",
-      message: `Chào mừng ${username} đã vào phòng chat ${room}`,
+      displayName: "Hệ thống",
+      message: `Chào mừng ${displayName} đã vào phòng chat ${room}`,
     });
     socket.emit("getID", socket.id);
     // gui ttin nhan
@@ -55,8 +76,8 @@ io.on("connection", (socket) => {
 
     // thong bao co 1 user vao phong cho cac user khac ttrong room
     socket.broadcast.to(room).emit("notify-new-user-connect", {
-      username: "Hệ thống",
-      message: `${username}  vừa vào phòng chat`,
+      displayName: "Hệ thống",
+      message: `${displayName}  vừa vào phòng chat`,
     });
     // ngat ket noi
     socket.on("disconnect", () => {
@@ -64,7 +85,7 @@ io.on("connection", (socket) => {
       removeUser(socket.id);
       const arrUser = getListUsersByRoom(roomID);
       io.to(roomID).emit("one-user-out", {
-        message: `${username} vừa rời khỏi phòng chat`,
+        message: `${displayName} vừa rời khỏi phòng chat`,
         data: arrUser,
       });
     });
--- controller/roomController.js
+++ controller/roomController.js
@@ -1,14 +1,21 @@
-const { Room } = require("./../models");
+const jsonwebtoken = require("jsonwebtoken");
+const { Room, User } = require("./../models");
 const apiRoom = {
   findAll: async (req, res) => {
-    const roomList = await Room.findAll();
+    const roomList = await Room.findAll({
+      include: {
+        model: User,
+      },
+    });
     res.send(roomList);
   },
   create: async (req, res) => {
-    const { name, hostId } = req.body;
+    const { name, token } = req.body;
+    const decode = jsonwebtoken.verify(token, "webmeet");
+    console.log(decode);
     const newRoom = await Room.create({
       name,
-      hostId,
+      hostId: decode.id,
     });
 
     res.send({ message: "Create Successfully", data: newRoom });
